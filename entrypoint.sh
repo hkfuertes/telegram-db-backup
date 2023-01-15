402d82fd347a68dcd1330eb9bd361476b9192fb5
hkfuertes <hkfuertes@gmail.com>
2023-01-15 02:08:44 +0100

    cosmetics

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,14 +21,14 @@ EOF
 cat <<EOF > /backup.sh
 #!/bin/bash
 
-DATE=\`date +"%d.%m.%Y"\`
+DATE=\`date +"%Y%m%d"\`
 TIME=\`date +"%d-%b-%Y %T"\`
 
 echo "[\${TIME}] Backing up \${DATABASE_HOST}:\${DATABASE_PORT}/\${DATABASE_NAME}..."
 /usr/bin/mysqldump --host \${DATABASE_HOST} --port \${DATABASE_PORT} -u \${DATABASE_USER} -p\${DATABASE_PASSWORD} -y \${DATABASE_NAME} > /tmp/backup_\${DATABASE_NAME}_latest.sql
-cd /tmp; zip backup_\${DATABASE_NAME}_\${DATE}.sql.zip backup_\${DATABASE_NAME}_latest.sql; cd /
+cd /tmp; zip \${DATABASE_NAME}_\${DATE}.sql.zip backup_\${DATABASE_NAME}_latest.sql; cd /
 echo "[\${TIME}] Copying to Telegram chat: \${CHAT_ID}..."
-sh /upload_to_telegram.sh /tmp/backup_\${DATABASE_NAME}_\${DATE}.sql.zip
+sh /upload_to_telegram.sh /tmp/\${DATABASE_NAME}_\${DATE}.sql.zip
 rm -rf /tmp/*.sql*
 echo "[\${TIME}] Backed and copied up!"
 EOF
